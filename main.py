d424862f3c5249a2e25aa0c2164f97040b7cf8b9
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2024-02-04 14:53:46 +0000

    added logic for distance and direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-pointsDict = {
+# Create dictionary of the coordinates
+pointsDictAll = {
    "CoordNum1" : {
       "x" : 28, 
       "y" : 42,
@@ -74,7 +74,7 @@ pointsDict = {
       "direction": "East"},
    "CoordNum19" : {
       "x" : 28, 
-      "y" : 46,
+      "y" : 26,
       "direction": "South"},
    "CoordNum20" : {
       "x" : 2, 
@@ -82,18 +82,68 @@ pointsDict = {
       "direction": "West"},
    }
 
-#print(pointsList)
+print("================================================================")
+print("================================================================")
 
+# Request User Input
 
 originNum = str(input("Please input a point number: --> "))
 arcDegree = int(input("Please input the degree of the arc: --> "))
 arcRadius = int(input("Please input the radius of the arc: --> "))
 
-pooX = pointsDict["CoordNum" + originNum]["x"]
-pooY = pointsDict["CoordNum" + originNum]["y"]
-pooDir = pointsDict["CoordNum"+ originNum]["direction"]
-
+pooCordNum = "CoordNum" + originNum
+pooX = pointsDictAll[pooCordNum]["x"]
+pooY = pointsDictAll[pooCordNum]["y"]
+pooDir = pointsDictAll[pooCordNum]["direction"]
 
 print(f"You have chosen Point of Origin {originNum} with arc degree of {arcDegree} and arc radius of {arcRadius}")
 print(f"This point is located at ({pooX},{pooY}) and points towards {pooDir}.")
 
+
+# Compute the hypotenuse between the all other points vs POO
+
+pointsDictTmp = pointsDictAll
+
+pointsDictTmp.pop(pooCordNum)
+
+# pointsDictTarget = {}
+
+pointsDictTarget = pointsDictTmp
+
+
+# Import math Library
+import math
+
+for i in pointsDictTmp.copy():
+   tpoCordNum = i
+   tpoX = pointsDictTmp[i]["x"]
+   tpoY = pointsDictTmp[i]["y"]
+   # print(f"{tpoCordNum} is ({tpoX},{tpoY})")
+   xDist = tpoX - pooX
+   yDist = tpoY - pooY
+   dist = round(math.sqrt(xDist**2 + yDist**2),2)
+   
+   print(f"Target {tpoCordNum} vs origin {pooCordNum} has a distance of {dist} and the radius is {arcRadius}")
+
+   # if dist <= arcRadius:
+   #    pointsDictTarget[tpoCordNum] = pointsDictTmp[tpoCordNum]
+   #    print(f"{tpoCordNum} has been added!")
+   #    print(pointsDictTarget)
+
+   if dist > arcRadius:
+      del pointsDictTmp[tpoCordNum]
+      print(f"{tpoCordNum} has been eliminated as dist > arcRadius!")
+   elif pooDir == "North" and yDist > 0:
+      print("target in the same North direction")
+   elif pooDir == "South" and yDist < 0:
+      print("target in the same South direction")
+   elif pooDir == "East" and xDist > 0:   
+      print("target in the same East direction")
+   elif pooDir == "West" and xDist < 0:   
+      print("target in the same East direction")
+   else:
+      print(pointsDictTmp)
+      del pointsDictTmp[tpoCordNum]
+      print(f"{tpoCordNum} has been eliminated because not in same direction!")
+
+print(pointsDictTmp)
